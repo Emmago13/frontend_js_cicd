cd8938f4f4b9acf5526870e72e24c8b9c1ada942
Emmanuel Gomez <80432917+Emmago13@users.noreply.github.com>
2021-09-01 21:29:41 -0300

    update footer message

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      This is the new feature of the page and this is a fix again
+      This is the new feature of the page and this is a fix again.
+      New footer here!
     </footer>
   );
 }
